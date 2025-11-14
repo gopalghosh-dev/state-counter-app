83d4b743502752bb67476884a4e9fa7e00036f67
Gopal Ghosh <gopalghosh.dev@gmail.com>
2025-11-14 19:09:51 +0530

    Expert Counter App Done

---
+++ src/components/ExpertCounter.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { CircleMinus, CirclePlus } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./shadcnui/button";
+
+const ExpertCounter = () => {
+	const [count, setCount] = useState(0);
+
+	const plusone = () => {
+		if (count <= 99) {
+			setCount(count + 1);
+		}
+	};
+
+	const minusone = () => {
+		if (count >= 1) {
+			setCount(count - 1);
+		}
+	};
+
+	const plusten = () => {
+		if (count <= 90) {
+			setCount(count + 10);
+		}
+	};
+
+	const minusten = () => {
+		if (count >= 1) {
+			setCount(count - 10);
+		}
+	};
+
+	return (
+		<div className="space-y-8">
+			<div className="text-center text-xl">
+				Counter Value:
+				<span className="font-semibold">{count}</span>
+			</div>
+
+			<div className="grid grid-cols-2 gap-4">
+				<Button
+					onClick={minusone}
+					variant={"destructive"}
+					className="cursor-pointer">
+					<CircleMinus /> Minus 1
+				</Button>
+
+				<Button
+					onClick={plusone}
+					className="cursor-pointer">
+					<CirclePlus />
+					Plus 1
+				</Button>
+
+				<Button
+					onClick={minusten}
+					variant={"destructive"}
+					className="cursor-pointer">
+					<CircleMinus /> Minus 10
+				</Button>
+
+				<Button
+					onClick={plusten}
+					className="cursor-pointer">
+					<CirclePlus />
+					Plus 10
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default ExpertCounter;
